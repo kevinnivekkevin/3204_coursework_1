52be812bd93c245fd216cc2aea56d3ac8d788682
Jeremy Chow <27985157+jeremyjevon@users.noreply.github.com>
2022-10-26 00:42:59 +0800

    Revert "Remove unused package"
    This reverts commit 550c9bc36ae70c33de3b82a4e4d04db6d4d129b0.

--- config/confluence/confluence_setup.sh
+++ config/confluence/confluence_setup.sh
@@ -12,6 +12,7 @@ apt-get install --no-install-recommends \
                 libpcap0.8 \
                 git \
                 make \
+                g++ \
                 python3-distutils \
                 python3-setuptools \
                 python3-venv \
