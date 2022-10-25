1c58c0130b719c54a4cf05eaa93704502a37e006
benjaminlimjt <2001209@sit.singaporetech.edu.sg>
2022-10-26 04:58:24 +0800

    remove redundant files

--- config/dashboard/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /home
-
-# Copy pre-configured project board into the project directory
-cp /vagrant/config/dashboard/project_board.ndjson .
-curl -X POST "http://127.0.0.1:5601/api/saved_objects/_import?createNewCopies=true" -H "kbn-xsrf: true" --form file=@project_board.ndjson
